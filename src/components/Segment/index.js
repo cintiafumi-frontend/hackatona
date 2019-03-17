1af23b0b7472040c3bc70b0184767d4db4ba7d1e
Danielle Nakatsu <danielle.n.nakatsu@accenture.com>
2019-03-17 11:22:52 -0300

    images

--- src/components/Segment/index.js
+++ src/components/Segment/index.js
@@ -6,71 +6,86 @@ const Segment = (props) => {
   switch (props.segment) {
     case 'seg1': return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+        
+          <i class="fas icon fa-utensils"></i>
         </Link>
       </div>
     )
     case 'seg2': return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+        
+          <i class="fas icon fa-dumbbell"></i>
         </Link>
       </div>
     )
     case 'seg3': return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+        
+          <i class="fas icon fa-building"></i>
+          
         </Link>
       </div>
     )
     case 'seg4': return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+        
+          <i class="fas icon fa-dumbbell"></i>
+          
         </Link>
       </div>
     )
     case 'seg5': return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+          
+          <i class="fas icon fa-building"></i>
+          
         </Link>
       </div>
     )
     case 'seg6': return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+          
+          <i class="fas icon fa-utensils"></i>
+          
         </Link>
       </div>
     )
     case 'seg7': return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+          
+          <i class="fas icon fa-building"></i>
+          
         </Link>
       </div>
     )
     case 'seg8': return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+          
+      <i class="fas icon fa-utensils"></i>
         </Link>
       </div>
     )
     case 'seg9': return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+          
+          <i class="fas icon fa-dumbbell"></i>
         </Link>
       </div>
     )   
     default: return (
       <div className='segment'>
-        <Link to={props.path}>
-          {/* imagem */}
+        <Link to={props.path} className='box'>
+          
+          <i class="fas icon fa-dumbbell"></i>
         </Link>
       </div>
     )
